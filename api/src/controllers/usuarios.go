15d59e91e2381b0b494c4be184171b21707d22d3
Igor Eloi Dias Branco <igoreloi1981@gmail.com>
2026-01-14 17:23:37 -0300

    add criar usuario

--- api/src/controllers/usuarios.go
+++ api/src/controllers/usuarios.go
@@ -1,9 +1,44 @@
 package controllers
 
-import "net/http"
+import (
+	"api/banco"
+	"api/models"
+	"api/repositorios"
+	"api/respostas"
+	"encoding/json"
+	"io"
+	"net/http"
+)
 
 func CriarUsuario(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Criando Usuario"))
+	corpoRequest, erro := io.ReadAll(r.Body)
+	if erro != nil {
+		respostas.Erro(w, http.StatusUnprocessableEntity, erro)
+		return
+	}
+
+	var usuario models.Usuario
+	if erro = json.Unmarshal(corpoRequest, &usuario); erro != nil {
+		respostas.Erro(w, http.StatusBadRequest, erro)
+		return
+	}
+
+	db, erro := banco.ConectarBanco()
+	if erro != nil {
+		respostas.Erro(w, http.StatusInternalServerError, erro)
+		return
+	}
+	defer db.Close()
+
+	repositorio := repositorios.RepositorioDeUsuarios(db)
+	usuario.ID, erro = repositorio.Criar(usuario)
+	if erro != nil {
+		respostas.Erro(w, http.StatusInternalServerError, erro)
+		return
+	}
+
+	respostas.JSON(w, http.StatusCreated, usuario)
+
 }
 
 func BuscarUsuarios(w http.ResponseWriter, r *http.Request) {
