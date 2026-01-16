a4ab258c300429ed7a5387c064219fc9e8c4bd31
Igor Eloi Dias Branco <igoreloi1981@gmail.com>
2026-01-16 14:05:40 -0300

    implementação de atualizar usuário

--- api/src/respostas/respostas.go
+++ api/src/respostas/respostas.go
@@ -12,8 +12,11 @@ func JSON(w http.ResponseWriter, statusCode int, dados interface{}) {
 
 	w.WriteHeader(statusCode)
 
-	if erro := json.NewEncoder(w).Encode(dados); erro != nil {
-		log.Fatal(erro)
+	if dados != nil {
+
+		if erro := json.NewEncoder(w).Encode(dados); erro != nil {
+			log.Fatal(erro)
+		}
 	}
 }
 
