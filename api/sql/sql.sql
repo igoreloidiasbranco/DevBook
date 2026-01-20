4417675a754d4c8e34895d6d7f33b1bbada9eea0
Igor Eloi Dias Branco <igoreloi1981@gmail.com>
2026-01-20 18:16:59 -0300

    Criação da tabela seguidores

--- api/sql/sql.sql
+++ api/sql/sql.sql
@@ -2,6 +2,7 @@ CREATE DATABASE IF NOT EXISTS devbook;
 USE devbook;
 
 DROP TABLE IF EXISTS usuarios;
+DROP TABLE IF EXISTS seguidores;
 
 CREATE TABLE usuarios (
     id INT AUTO_INCREMENT PRIMARY KEY,
@@ -10,4 +11,18 @@ CREATE TABLE usuarios (
     email VARCHAR(50) UNIQUE NOT NULL,
     senha VARCHAR(100) NOT NULL,
     criado_em TIMESTAMP DEFAULT CURRENT_TIMESTAMP()
+) ENGINE=INNODB;
+
+CREATE TABLE seguidores(
+    usuario_id int not null,
+    FOREIGN KEY (usuario_id)
+    REFERENCES usuarios(id)
+    ON DELETE CASCADE,
+
+    seguidor_id int not null,
+    FOREIGN KEY (seguidor_id)
+    REFERENCES usuarios(id)
+    ON DELETE CASCADE,
+
+    PRIMARY KEY(usuario_id, seguidor_id)
 ) ENGINE=INNODB;
