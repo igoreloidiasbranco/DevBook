d1f3ae1cacfa056b37a13ec0e37b9fca20695ba4
Igor Eloi Dias Branco <igoreloi1981@gmail.com>
2026-01-21 16:54:49 -0300

    add Busca de Seguidores

--- api/src/repositorios/usuarios.go
+++ api/src/repositorios/usuarios.go
@@ -169,3 +169,33 @@ func (repositorio usuarios) PararDeSeguir(usuarioID, seguidorID uint64) error {
 
 	return nil
 }
+
+func (repositorio usuarios) BuscarSeguidores(usuarioID uint64) ([]models.Usuario, error) {
+	linhas, erro := repositorio.db.Query(`
+	select u.id, u.nome, u.nick, u.email, u.criado_em 
+	from usuarios u inner join seguidores s on u.id = s.seguidor_id where s.usuario_id = ?
+	`, usuarioID)
+	if erro != nil {
+		return nil, erro
+	}
+	defer linhas.Close()
+
+	var usuarios []models.Usuario
+	for linhas.Next() {
+		var usuario models.Usuario
+
+		if erro = linhas.Scan(
+			&usuario.ID,
+			&usuario.Nome,
+			&usuario.Nick,
+			&usuario.Email,
+			&usuario.CriadoEm,
+		); erro != nil {
+			return nil, erro
+		}
+
+		usuarios = append(usuarios, usuario)
+	}
+
+	return usuarios, nil
+}
