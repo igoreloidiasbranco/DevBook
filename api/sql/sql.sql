08d849afbe4cfa2899192a20b85cce091523c8f2
Igor Eloi Dias Branco <igoreloi1981@gmail.com>
2026-01-22 18:12:10 -0300

    Criação da struct e tabela de publicações

--- api/sql/sql.sql
+++ api/sql/sql.sql
@@ -1,8 +1,10 @@
 CREATE DATABASE IF NOT EXISTS devbook;
 USE devbook;
 
-DROP TABLE IF EXISTS usuarios;
+DROP TABLE IF EXISTS publicacoes;
 DROP TABLE IF EXISTS seguidores;
+DROP TABLE IF EXISTS usuarios;
+
 
 CREATE TABLE usuarios (
     id INT AUTO_INCREMENT PRIMARY KEY,
@@ -25,4 +27,16 @@ CREATE TABLE seguidores(
     ON DELETE CASCADE,
 
     PRIMARY KEY(usuario_id, seguidor_id)
+) ENGINE=INNODB;
+
+CREATE TABLE publicacoes (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    titulo VARCHAR(50) NOT NULL,
+    conteudo VARCHAR(300) NOT NULL,
+    autor_id INT NOT NULL,
+    FOREIGN KEY (autor_id)
+    REFERENCES usuarios(id)
+    ON DELETE CASCADE,
+    curtidas INT DEFAULT 0,
+    criado_em TIMESTAMP DEFAULT CURRENT_TIMESTAMP()
 ) ENGINE=INNODB;
