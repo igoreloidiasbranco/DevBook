d63c3fae24fdba062253b0235f9d2ebf145f7e0f
Igor Eloi Dias Branco <igoreloi1981@gmail.com>
2026-01-23 12:53:08 -0300

    add Criar Publicação

--- api/src/controllers/publicacoes.go
+++ api/src/controllers/publicacoes.go
@@ -1,9 +1,56 @@
 package controllers
 
-import "net/http"
+import (
+	"api/autenticacao"
+	"api/banco"
+	"api/models"
+	"api/repositorios"
+	"api/respostas"
+	"encoding/json"
+	"io"
+	"net/http"
+	"time"
+)
 
 func CriarPublicacao(w http.ResponseWriter, r *http.Request) {
 
+	usuarioID, erro := autenticacao.ExtrairUsuarioID(r)
+	if erro != nil {
+		respostas.Erro(w, http.StatusUnauthorized, erro)
+		return
+	}
+
+	corpoRequest, erro := io.ReadAll(r.Body)
+	if erro != nil {
+		respostas.Erro(w, http.StatusUnprocessableEntity, erro)
+		return
+	}
+
+	var publicacao models.Publicacao
+	if erro = json.Unmarshal(corpoRequest, &publicacao); erro != nil {
+		respostas.Erro(w, http.StatusBadRequest, erro)
+		return
+	}
+
+	publicacao.AutorID = usuarioID
+
+	db, erro := banco.ConectarBanco()
+	if erro != nil {
+		respostas.Erro(w, http.StatusInternalServerError, erro)
+		return
+	}
+
+	defer db.Close()
+
+	repositorio := repositorios.RepositorioDePublicacoes(db)
+	publicacao.CriadaEm = time.Now()
+	publicacao.ID, erro = repositorio.Criar(publicacao)
+	if erro != nil {
+		respostas.Erro(w, http.StatusInternalServerError, erro)
+		return
+	}
+
+	respostas.JSON(w, http.StatusCreated, publicacao)
 }
 
 func BuscarPublicacoes(w http.ResponseWriter, r *http.Request) {
